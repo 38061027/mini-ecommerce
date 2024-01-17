4357cbd59e1bc200295ca41251e3e341ac718eda
38061027 <joseaugustogomessantos12@gmail.com>
2024-01-17 19:10:00 -0300

    estilizando e criando api de produtos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { resetFakeAsyncZone } from '@angular/core/testing';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  products!:any[]
+
+  constructor(private service: ProductsService){}
+
+  getProducts(){
+    this.service.getProducts().subscribe(
+      res => this.products = res
+    )
+  }
+
+  ngOnInit(): void {
+    this.getProducts()
+  }
+  
 
 }
